1b95065425945e21e75cf18722f394111e9188b8
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-11-21 19:12:05 +0300

    done

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,18 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, only: [:id, :name]
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
+    if cheese
     render json: cheese
+    else
+      # custom error
+      render json: {error: "cheese not found"}, status: 404
+    end
   end
 
 end
